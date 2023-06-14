575ba75c51757f1d422d5159b83cce985ca0810a
Vache Apinyan <apinyan.v@gmail.com>
2023-06-14 17:08:36 +0400

    feat(*): add clean, and html webpack plugins and hashing filname to production

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const mode = process.env.NODE_ENV
 
@@ -6,7 +8,12 @@ module.exports = {
     mode,
     entry: "./src/index.js",
     output: {
-        filename: "bundle.js",
+        filename: (mode === "production") ? "[hash].js" : "bundle.js",
         path: path.resolve(__dirname, "dist")
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin()
+    ],
 }
